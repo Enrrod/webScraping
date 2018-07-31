adbef5c5adc89a7038176ad68ac0af66a2032723
Enrrod <enrique.rodsebastian@gmail.com>
2018-07-31 11:06:59 +0200

    Changes in cript for searching by url directly and avoid fill and click

--- firstPageLinks.py
+++ firstPageLinks.py
@@ -8,30 +8,33 @@ import time
 def firstSearches(search):
     hour = time.strftime("%H:%M:%S")
     date = time.strftime("%d/%m/%y")
+    date = date.replace('/','-')
 
     browser = Browser('chrome')
     browser.driver.set_window_size(640, 480)
     browser.visit('https://www.google.com')
-
-    search_bar_xpath = '//*[@id="lst-ib"]'
-    search_button_xpath = '//*[@id="tsf"]/div[2]/div[3]/center/input[1]'
-
-    search_bar = browser.find_by_xpath(search_bar_xpath)[0]
-    search_button = browser.find_by_xpath(search_button_xpath)[0]
-
-    search_bar.fill(search)
-    search_button.click()
+    keyWords = search.split()
+    query = ''
+    for i in range(len(keyWords)):
+        if i != len(keyWords) - 1:
+            query = query + keyWords[i] + '+'
+        else:
+            query = query + keyWords[i]
+    url = 'https://www.google.es/search?q=' + query
+    browser.visit(url)
 
     search_results_xpath = '//h3[@class="r"]/a'
     search_results = browser.find_by_xpath(search_results_xpath)
+
+    scraped_data = []
     for search_result in search_results:
         title = search_result.text.encode('utf8')
         link = search_result["href"]
         scraped_data.append((title, link))
 
     df = pd.DataFrame(data=scraped_data, columns=["Title", "Link"])
-    df.to_csv("/home/enrique/Escritorio/links_" + date + "_" + hour + ".csv")
+    df.to_csv('/home/enrique/Documentos/Noticias automaticas/links_' + date + '_' + hour + '.csv')
 
 
 if __name__=="__main__":
-    firstSearches('noticias ciencia')
+    firstSearches('ultimas noticias viajes espaciales')
