f9dc37f4a9f4880acab681a2321b20ae82027f34
Enrrod <enrique.rodsebastian@gmail.com>
2018-08-01 11:59:22 +0200

    First page links function added for a search on scholar

--- firstPageLinks.py
+++ firstPageLinks.py
@@ -12,7 +12,6 @@ def firstSearches(search):
 
     browser = Browser('chrome')
     browser.driver.set_window_size(640, 480)
-    browser.visit('https://www.google.com')
     keyWords = search.split()
     query = ''
     for i in range(len(keyWords)):
@@ -33,8 +32,38 @@ def firstSearches(search):
         scraped_data.append((title, link))
 
     df = pd.DataFrame(data=scraped_data, columns=["Title", "Link"])
-    df.to_csv('/home/enrique/Documentos/Noticias automaticas/links_' + date + '_' + hour + '.csv')
+    df.to_csv('/home/enrique/Documentos/Busqueda automatica/links_' + date + '_' + hour + '.csv')
+
+
+def firstScholarSearches(search):
+    hour = time.strftime("%H:%M:%S")
+    date = time.strftime("%d/%m/%y")
+    date = date.replace('/','-')
+
+    browser = Browser('chrome')
+    browser.driver.set_window_size(640, 480)
+    keyWords = search.split()
+    query = ''
+    for i in range(len(keyWords)):
+        if i != len(keyWords) - 1:
+            query = query + keyWords[i] + '+'
+        else:
+            query = query + keyWords[i]
+    url = 'https://scholar.google.es/scholar?hl=es&as_sdt=0%2C5&as_ylo=2018&q=' + query + '&oq='
+    browser.visit(url)
+
+    search_results_xpath = '//h3[@class="gs_rt"]/a'
+    search_results = browser.find_by_xpath(search_results_xpath)
+
+    scraped_data = []
+    for search_result in search_results:
+        title = search_result.text.encode('utf8')
+        link = search_result["href"]
+        scraped_data.append((title, link))
+
+    df = pd.DataFrame(data=scraped_data, columns=["Title", "Link"])
+    df.to_csv('/home/enrique/Documentos/Busqueda automatica/linksScholar_'+ query + '_' + date + '_' + hour + '.csv')
 
 
 if __name__=="__main__":
-    firstSearches('ultimas noticias viajes espaciales')
+    firstScholarSearches('field theories of mind')
